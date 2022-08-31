3b939c62fd7d5499e87c275a7d5748babe503f6a
hgq2002 <h15629526027@163.com>
2022-08-31 14:26:50 +0800

    forgot to modify

--- src/main/java/com/ncamc/schedule/Automatically.java
+++ src/main/java/com/ncamc/schedule/Automatically.java
@@ -33,27 +33,31 @@ public class Automatically implements Runnable {
     @SneakyThrows
     @Override
     public void run() {
-        System.out.println("我是一个定时任务");
-        List<User> users = userMapper.selectList(null);
-        for (User user : users) {
-            String status = user.getStatus();
-            if (status.equals("0")){
-                System.out.println(user.getUsername()+"账户没有被锁定");
+        try {
+            System.out.println("我是一个定时任务");
+            List<User> users = userMapper.selectList(null);
+            for (User user : users) {
+                String status = user.getStatus();
+                if (status.equals("0")){
+                    System.out.println(user.getUsername()+"账户没有被锁定");
+                }
+                if (status.equals("1")) {
+                    System.out.println(user.getUsername()+"账户被锁定");
+                    System.out.println("正在自动解锁该账户");
+                    System.out.println("正在重置状态");
+                    user.setStatus("0");
+                    userMapper.updateStatusById(user.getStatus(),user.getId());
+                    System.out.println("正在重置登陆次数");
+                    user.setNumber(0);
+                    userMapper.chongZhiNumberById(user.getNumber(),user.getId());
+                    System.out.println("正在重置密码");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    userMapper.updatePasswordById(user.getPassword(),user.getId());
+                    System.out.println(user.getUsername()+"账户重置成功，欢迎再次使用");
+                }
             }
-            if (status.equals("1")) {
-                System.out.println(user.getUsername()+"账户被锁定");
-                System.out.println("正在自动解锁该账户");
-                System.out.println("正在重置状态");
-                user.setStatus("0");
-                userMapper.updateStatusById(user.getStatus(),user.getId());
-                System.out.println("正在重置登陆次数");
-                user.setNumber(0);
-                userMapper.chongZhiNumberById(user.getNumber(),user.getId());
-                System.out.println("正在重置密码");
-                user.setPassword(passwordEncoder.encode("123"));
-                userMapper.updatePasswordById(user.getPassword(),user.getId());
-                System.out.println(user.getUsername()+"账户重置成功，欢迎再次使用");
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
